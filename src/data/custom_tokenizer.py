e7bfbbc474e5b37e9327e3cb75fcb3263188b59f
HwangIsAce <esteen196@naver.com>
2024-12-27 22:58:52 +0900

    feat: implement basic dataloader

---
+++ src/data/custom_tokenizer.py
@@ -0,0 +1,41 @@
+import re
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+    
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s, i in vocab.items()}
+
+    def encode(self, text):
+        preprocessd = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessd = [
+            item.strip() for item in preprocessd if item.strip()
+        ]
+        preprocessd = [item if item in self.str_to_int 
+                       else "<|unk|>" for item in preprocessd]
+        ids = [self.str_to_int[s] for s in preprocessd]
+        return ids
+    
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
+        return text
