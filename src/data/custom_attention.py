fe02ef3a0b781caec1de927be156ff5936b1bb00
HwangIsAce <esteen196@naver.com>
2024-12-30 17:52:20 +0900

    feat: implement multi-head attention mechanism

--- src/data/custom_attention.py
+++ src/data/custom_attention.py
@@ -40,3 +40,105 @@ class SelfAttention_v2(nn.Module):
         context_vec = attn_weights @ values
         return context_vec
     
+class CausalAttention(nn.Module):
+    def __init__(
+            self, d_in, d_out, context_length,
+            dropout, qkv_bias=False
+    ):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            'mask',
+            torch.triu(torch.ones(context_length, context_length), diagonal=1) # to register untrained tensor as model properties. these tensors are includeed in the model's state, but are maintained as constant values that are not updated during training
+        )
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1, 2) # generally, index 0 indicates the batch size.
+        attn_scores.masked_fill_(
+            self.mask.bool()[:num_tokens, :num_tokens], -torch.inf # used by importing self.mask from register_buffer
+        )
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        attn_weights = self.dropout(attn_weights)
+
+        context_vec = attn_weights @ values
+        return context_vec
+
+class MultiheadAttentionWrapper(nn.Module):
+    def __init__(
+            self, d_in, d_out, context_length,
+            dropout, num_heads, qkv_bias=False
+    ):
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [CausalAttention(
+                d_in, d_out, context_length, dropout, qkv_bias
+            )
+            for _ in range(num_heads)
+            ]
+        )
+
+    def forward(self, x):
+        return torch.cat([head(x) for head in self.heads], dim=-1)
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(
+            self, d_in, d_out, context_length,
+            dropout, num_heads, qkv_bias=False
+    ):
+        super().__init__()
+        assert (d_out % num_heads == 0), \
+        "d_out must be divisible by num_heads"
+
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads ## importance
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_in, d_out)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        )
+    def forward(self, x): # q,k,v -> view -> transpose -> scoring(q,k calculation) -> masking -> weight update -> concat 
+        b, num_tokens, d_in = x.shape
+        keys = self.W_key(x)
+        quiries = self.W_query(x)
+        values = self.W_value(x)
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+        quiries = quiries.view(b, num_tokens, self.num_heads, self.head_dim) # (b, num_tokens, self.num_heads, self.head_dim)
+
+        keys = keys.transpose(1, 2) # (b, self.num_heads, num_tokens, self.head_dim)
+        queries = quiries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        attn_scores = queries @ keys.transpose(2, 3) # (b, self.num_heads, num_tokens, self.head_dim) @  (b, self.num_heads, self.head_dim, num_tokens)
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens] 
+
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        attn_weights = self.dropout(attn_weights)
+
+        context_vec = (attn_weights @ values).transpose(1, 2) # (b, num_tokens, self.num_heads, self.head_dim)
+        context_vec = context_vec.contiguous().view(
+            b, num_tokens, self.d_out
+        )
+
+        context_vec = self.out_proj(context_vec)
+        return context_vec
