d1c2cabc63bcf03959c9188f51342ad801a52081
HwangIsAce <esteen196@naver.com>
2025-01-06 14:29:01 +0900

    feat: implement FeedForward class with GeLU function and DNN sample class with residual connection

--- src/data/custom_model.py
+++ src/data/custom_model.py
@@ -15,7 +15,29 @@ class DummyLayerNorm(nn.Module):
 
     def forward(self, x):
         return x
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) *
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
     
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__inint__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg['emb_dim']),
+            GELU(),
+            nn.linear(4 * cfg['emb_dim'], cfg['emb_dim'])
+        )
+
+    def forward(self, x):
+        return self.layer(x)
+
 class LayerNorm(nn.Module):
     def __init__(self, emb_dim):
         super().__init__()
@@ -29,6 +51,27 @@ class LayerNorm(nn.Module):
         norm_x = (x - mean) / torch.sqrt(var + self.eps)
         return self.scale * norm_x + self.shift
     
+class ExampleDeepNeuralNetwork(nn.Module):
+    def __init__(self, layer_sizes, use_shortcut):
+        super().__init__()
+        self.use_shortcut = use_shortcut
+        self.layers = nn.ModuleList([
+            nn.Sequential(nn.Linear(layer_sizes[0], layer_sizes[1]), GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[1], layer_sizes[2]), GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[2], layer_sizes[3]), GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[3], layer_sizes[4]), GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[4], layer_sizes[5]), GELU()),
+        ])
+    
+    def forward(self, x):
+        for layer in self.layers:
+            layer_output = layer(x)
+            if self.use_shortcut and x.shape == layer_output.shape:
+                x = x + layer_output
+            else:
+                x = layer_output
+        return x
+
 class DummyGPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
