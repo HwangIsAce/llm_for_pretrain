5cceca83017c7f5ca4d7e0d4f70b6215473108d3
HwangIsAce <esteen196@naver.com>
2025-01-24 16:18:01 +0900

    feat: implement code for training model

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,5 +1,7 @@
 import tiktoken
 import torch
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 def text_to_token_ids(text, tokenizer):
     encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
@@ -10,3 +12,18 @@ def token_ids_to_text(token_ids, tokenizer):
     flat = token_ids.squeeze(0)
     return tokenizer.decode(flat.tolist())
 
+def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+    ax1.plot(epochs_seen, train_losses, label="training loss")
+    ax1.plot(
+        epochs_seen, val_losses, linestyle="-.", label="validation loss"
+    )
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.legend(loc="upper right")
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax2 = ax1.twiny()
+    ax2.plot(tokens_seen, train_losses, alpha=0)
+    ax2.set_xlabel("Tokens seen")
+    fig.tight_layout()
+    plt.show()
